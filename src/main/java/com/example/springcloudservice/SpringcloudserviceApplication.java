ebf824782896584e3bff4657d32ed119124d1635
luozuolin <727265606@qq.com>
2018-05-22 14:02:22 +0800

    项目初始化

---
+++ src/main/java/com/example/springcloudservice/SpringcloudserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserviceApplication.class, args);
+	}
+}
